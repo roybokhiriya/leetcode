4fa9bbdb7028c36056c0bdd4c0307624c28781eb
roybokhiriya <111169490+roybokhiriya@users.noreply.github.com>
2025-05-12 12:46:57 +0530

    Time: 8 ms (43.4%), Space: 40.4 MB (64.16%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Solution {
 
-    // Recursive method to calculate Fibonacci number
     public int fib(int n) {
         if (n <= 1) {
             return n;
@@ -12,9 +11,9 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // Number of terms to print
+        int n = sc.nextInt(); 
 
-        Solution obj = new Solution(); // Create object to call non-static method
+        Solution obj = new Solution(); 
 
         System.out.println("Fibonacci Series up to " + n + " terms:");
 
