888dc45190c471d809414406a5a90f6fbb3a5c95
roybokhiriya <111169490+roybokhiriya@users.noreply.github.com>
2025-05-19 14:07:20 +0530

    Time: 4 ms (91.64%), Space: 46.2 MB (9%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[][] imageSmoother(int[][] img) {
-        final int m = img.length, n = img[0].length;
-        final int[][] res = new int[m][n];
+        int m = img.length, n = img[0].length;
+        int[][] res = new int[m][n];
 
-        for (int i = 0; i < m; ++i) {
-            int iMin = Math.max(i - 1, 0);
-            int iMax = Math.min(i + 1, m - 1);
-            for (int j = 0; j < n; ++j) {
-                int jMin = Math.max(j - 1, 0);
-                int jMax = Math.min(j + 1, n - 1);
+        for (int i = 0; i < m; i++) {
+            int i1 = Math.max(i - 1, 0), i2 = Math.min(i + 1, m - 1);
+            for (int j = 0; j < n; j++) {
+                int j1 = Math.max(j - 1, 0), j2 = Math.min(j + 1, n - 1);
                 int sum = 0, count = 0;
 
-                for (int x = iMin; x <= iMax; ++x) {
-                    for (int y = jMin; y <= jMax; ++y) {
+                for (int x = i1; x <= i2; x++) {
+                    for (int y = j1; y <= j2; y++) {
                         sum += img[x][y];
-                        ++count;
+                        count++;
                     }
                 }
 
@@ -25,3 +23,4 @@ class Solution {
         return res;
     }
 }
+
