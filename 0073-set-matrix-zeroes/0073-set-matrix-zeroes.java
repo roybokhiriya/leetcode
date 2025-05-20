fa7cb0a401cb10021c3763bbce3e3998f4824b40
ronak bokhiriya <111169490+roybokhiriya@users.noreply.github.com>
2025-05-20 11:26:37 +0530

    Time: 1 ms (72.87%), Space: 45.5 MB (82.95%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -4,12 +4,10 @@ class Solution {
         int col = matrix[0].length;
         int[][] ans = new int[row][col];
 
-        // Copy original matrix
         for (int i = 0; i < row; i++)
             for (int j = 0; j < col; j++)
                 ans[i][j] = matrix[i][j];
 
-        // Mark zeros in ans
         for (int i = 0; i < row; i++)
             for (int j = 0; j < col; j++)
                 if (matrix[i][j] == 0) {
@@ -17,7 +15,6 @@ class Solution {
                     for (int r = 0; r < row; r++) ans[r][j] = 0;
                 }
 
-        // Copy back to original matrix
         for (int i = 0; i < row; i++)
             for (int j = 0; j < col; j++)
                 matrix[i][j] = ans[i][j];
