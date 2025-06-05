c1b3ed9f76d07cfe524d3c677cbe4d87f80f1e8e
ronak bokhiriya <111169490+roybokhiriya@users.noreply.github.com>
2025-06-05 13:10:25 +0530

    Time: 1 ms (77.21%), Space: 41.8 MB (35.95%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+
+    private String build(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '#') {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
